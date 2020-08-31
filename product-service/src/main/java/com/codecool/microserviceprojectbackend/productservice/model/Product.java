a7fa82c2b6a2b710b01cb4af81f13734158dab99
Tóth Máté <tothmate911@gmail.com>
2020-08-31 14:16:16 +0200

    create product class

---
+++ product-service/src/main/java/com/codecool/microserviceprojectbackend/productservice/model/Product.java
@@ -0,0 +1,33 @@
+package com.codecool.microserviceprojectbackend.productservice.model;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private Long productId;
+
+    private String name;
+
+    private String description;
+
+    private String imageUrl;
+
+    private BigDecimal price;
+
+    private String category;
+
+    private Long userId;
+}
