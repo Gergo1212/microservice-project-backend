2f611998661ccaad80cb5da17deace641c9344b3
Tóth Máté <tothmate911@gmail.com>
2020-09-01 09:59:11 +0200

    add methods to ProductController

--- product-service/src/main/java/com/codecool/microserviceprojectbackend/productservice/dao/ProductDaoDB.java
+++ product-service/src/main/java/com/codecool/microserviceprojectbackend/productservice/dao/ProductDaoDB.java
@@ -1,4 +1,32 @@
 package com.codecool.microserviceprojectbackend.productservice.dao;
 
+import com.codecool.microserviceprojectbackend.productservice.model.Product;
+
+import java.util.List;
+
 public class ProductDaoDB implements ProductDao {
+    @Override
+    public List<Product> getAllProducts() {
+        return null;
+    }
+
+    @Override
+    public Product getProduct(Long id) {
+        return null;
+    }
+
+    @Override
+    public Product addProduct(Product product) {
+        return null;
+    }
+
+    @Override
+    public Product deleteProduct(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Product> searchProductsByName(String name) {
+        return null;
+    }
 }
